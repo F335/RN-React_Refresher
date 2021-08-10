bdeb851e1ab53baa308e137fede93f62203c877a
F335 <49539551+F335@users.noreply.github.com>
2021-08-11 00:19:33 +0200

    Add code up to lecture 337 - Working with Props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import GoalList from "./components/GoalList";
 import "./App.css";
 
 const App = () => {
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg1", text: "Learn all about the Course Main Topic" },
+    { id: "cg1", text: "Help other students in the Course Q&A" },
+  ];
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
