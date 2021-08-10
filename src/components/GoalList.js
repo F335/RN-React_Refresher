cb52bfd4ee3c850aed50b4a0f3da9a7e99e27c3f
F335 <49539551+F335@users.noreply.github.com>
2021-08-10 23:52:12 +0200

    Fix the formatting

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './GoalList.css';
+import "./GoalList.css";
 
 const GoalList = () => {
-    return (
-        <ul className="goal-list">
-            <li>Finish the Course</li>
-            <li>Learn all about the Course Main Topic</li>
-            <li>Help other students in the Course Q&amp;A section</li>
-        </ul>
-    );
-}
+  return (
+    <ul className="goal-list">
+      <li>Finish the Course</li>
+      <li>Learn all about the Course Main Topic</li>
+      <li>Help other students in the Course Q&amp;A section</li>
+    </ul>
+  );
+};
 
 export default GoalList;
